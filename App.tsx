d4b3820d2aceb5986adc53d7bf4b0e986e68f02c
chokakun <80216ymshhccedutw@gmail.com>
2025-12-07 11:54:09 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { InputGroup } from './components/InputGroup';
 import { ResultCard } from './components/ResultCard';
 import { LampSelector } from './components/LampSelector';
